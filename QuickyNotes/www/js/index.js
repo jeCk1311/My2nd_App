f89585f85256631d8b07499cff915c1cc040464f
jeCk Adlaper Onuiqa <30889855+jeCk1311@users.noreply.github.com>
2017-11-17 11:56:39 +0800

    Update index.js

--- QuickyNotes/www/js/index.js
+++ QuickyNotes/www/js/index.js
@@ -1,36 +1,230 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
+$(document).one('pageinit', function(){
+   
+    showNotes();
+
+    
+    $('#submitNote').on('tap', addNotes);
+
+   
+    $('#submitEditNote').on('tap', editNote);
+
+    
+    $('#note_List').on('tap','#editLink', setCurrentVals);
+
+    
+    $('#deleteNote').on('tap', deleteNote);
+
+   
+    function getNotesObject(){
+        
+       
+        var notes = new Array();
+
+       
+        var currentNotes = localStorage.getItem('notes');
+
+        
+        if (currentNotes != null){
+
+            
+            var notes = JSON.parse(currentNotes);
+        }
+
+       
+        return notes.sort(function(a,b) {return new Date(b.noteTime) - new Date(a.noteTime)});   
+    }
+
+  
+    function addNotes(){
+        
+        
+        var noteDescription = $('#noteDescription').val();
+        var noteText = $('#addNote').val();
+        var currentdate = new Date(); 
+        var noteDate = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() 
+                
+        var noteTime = currentdate.getHours() + ":" + currentdate.getMinutes()
+
+       
+        var note = {
+            noteDescription: noteDescription,
+            noteText: noteText,
+            noteDate: noteDate,
+            noteTime: noteTime
+            
+
+        };
+
+        var notes = getNotesObject();
+
+        
+        notes.push(note);
+
+      
+        localStorage.setItem('notes', JSON.stringify(notes));
+
+       
+        window.location.href = "index.html";
+
+        return false;
+    
+    }
+    
+
+   
+    function showNotes(){
+        var notes = getNotesObject();
+
+        if (notes != '' && notes != null ){
+
+            for(var i=0; i<notes.length; i++){
+                
+                $('#note_List').append('<li class="ui-body-inherit"><a href="#EditNotePage" id="editLink" data-1="'+ notes[i]['noteDescription']+'" data-2="'+notes[i]['noteText']+'" data-3="'+notes[i]['noteTime']+'" data-4="'+notes[i]['noteDate']+'"><h3>'+ notes[i]['noteDescription']+'</h3>'+
+                '<p>'+ notes[i]['noteText'] +'</p> <p class="ui-li-aside">'+ notes[i]['noteDate'] + " @ " + notes[i]['noteTime'] +'</p></a></li>').listview('refresh');
+
+            }
+
+            
+            $('#home').bind('pageinit', function(){
+              
+                $('#note_List').listview('refresh');
+            });
+
+        }   
+    
+    }
+
+  
+    function setCurrentVals(){
+        
+        localStorage.setItem('currentNoteDescription', $(this).data('1'));
+        localStorage.setItem('currentNoteText', $(this).data('2'));
+        localStorage.setItem('currentNoteTime', $(this).data('3'));
+        localStorage.setItem('currentNoteDate', $(this).data('4'));
+        
+
+       
+        $('#noteDescriptionEdit').val(localStorage.getItem('currentNoteDescription'));
+        $('#noteEdit').val(localStorage.getItem('currentNoteText'));
+
+    }
+
+    function deleteNote (){
+        var procceed = confirm("Are you sure?");
+        if(procceed){
+
+
+            
+            var currentNoteDescription = $('#noteDescriptionEdit').val();
+            var currentNoteText = $('#noteEdit').val();
+
+            
+            var notes = getNotesObject();
+
+            
+            for(var i=0; i<notes.length; i++){
+                if(notes[i].noteDescription == currentNoteDescription && 
+                    notes[i].noteText == currentNoteText){
+
+                   
+                    notes.splice(i, 1);
+
+
+
+                }
+
+                localStorage.setItem('notes', JSON.stringify(notes));
+
+
+            }
+            getNotesObject();
+            
+            window.location.href = "index.html";
+
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    
+    function editNote(){
+       
+        currentNoteDescription = localStorage.getItem('currentNoteDescription');
+        currentNoteText = localStorage.getItem('currentNoteText');
+        currentNoteTime = localStorage.getItem('currentNoteTime');
+        currentNoteDate = localStorage.getItem('currentNoteDate');
+
+        var notes = getNotesObject();
+
+        
+        for (var i =0; i<notes.length; i++){
+
+            if(notes[i].noteDescription == currentNoteDescription && notes[i].noteText == currentNoteText && notes[i].noteTime == currentNoteTime && notes[i].noteDate == currentNoteDate){
+                
+                notes.splice(i, 1);
+            }
+
+            localStorage.setItem('notes', JSON.stringify(notes));
+        }
+
+        
+        var noteDescription = $('#noteDescriptionEdit').val();
+        var noteText = $('#noteEdit').val();
+        var currentdate = new Date(); 
+        var noteDate = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() 
+                
+        var noteTime = currentdate.getHours() + ":" + currentdate.getMinutes()
+
+       
+        var update_notes = {
+            noteDescription: noteDescription,
+            noteText: noteText,
+            noteDate: noteDate,
+            noteTime: noteTime
+        };
+
+        
+
+         
+        notes.push(update_notes);
+
+        
+        localStorage.setItem('notes', JSON.stringify(notes));
+
+         
+        window.location.href = "index.html";
+
+        return false;
+
+    }
+
+    
+});
+
+
 var app = {
-    // Application Constructor
+    
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        this.bindEvents();
     },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
+    
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+        app.receivedEvent('deviceready');
     },
-
-    // Update DOM on a Received Event
+    
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
